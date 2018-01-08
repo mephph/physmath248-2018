3b563f1256b484fa0a02b783823827d9ae040867
fherwig <fherwig@uvic.ca>
2018-01-08 17:47:25 +0000

    update Jan8 class

---
+++ examples/example.sh
@@ -0,0 +1,13 @@
+# this is a comment
+# if you make this file executable you can use it like a program
+# each line will be executed one after another as if you typed
+# it in on the command line
+#
+# chmod u+x example.sh
+# is the command that would make this script an executable
+# then just type example.sh on the command line and hit return
+ls
+mkdir example_dir
+cd example_dir
+touch example.txt
+pwd
